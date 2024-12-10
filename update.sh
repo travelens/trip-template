ae551f7fc08cb803939cf6392d10d08c33053b8d
Christine Tham <chris.tham@hellotham.com>
2024-12-10 17:58:33 +1100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cp -a ../trip-template/{.github,.vscode,.gitignore,.prettierignore,*config*,LICENSE,update.sh} .
-cp -a ../trip-template/src/{components,layouts,pages,env.d.ts} src
+cp -a ../trip-template/src/{components,layouts,pages,content.config.ts} src
