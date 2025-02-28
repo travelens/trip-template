529b4677cafb7f453ed802b60ef2aca7463b45dd
Christine Tham <chris.tham@hellotham.com>
2025-02-28 18:19:56 +1100

    Modified to use common submodule

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import UnoCSS from 'unocss/astro'
 import markdoc from '@astrojs/markdoc'
 import alpine from '@astrojs/alpinejs'
 import { defineConfig } from 'astro/config'
-import { SiteMetadata } from 'astro-travelens/config'
+import { SiteMetadata } from './src/common/config.ts'
 
 // https://astro.build/config
 export default defineConfig({
