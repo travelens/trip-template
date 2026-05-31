80c58e9cd7bfb69507ff16c911ac6e92bb590dcb
Christine Tham <chris.tham@hellotham.com>
2026-05-31 20:46:28 +1000

    feat: update GitHub Actions workflow and improve script comments

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 cp -a ../trip-template/{.github,.vscode,.gitignore,.prettierignore,*config*,LICENSE,update.sh} .
 cp -a ../trip-template/src/{pages,content.config.ts} src
-rm prettier.config.cjs
+# rm prettier.config.cjs
 node -e "
   const fs = require('fs');
   const template = JSON.parse(fs.readFileSync('../trip-template/package.json', 'utf8'));
